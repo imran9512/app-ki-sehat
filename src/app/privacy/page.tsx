44f80475b2112163548f188909bf34cf141aa59e
imran9512 <48657619+imran9512@users.noreply.github.com>
2025-07-20 15:36:33 +0500

    Update page.tsx

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,13 @@
+// src/app/contact/page.tsx
+import { useState } from 'react';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Privacy</h1>
+      <p>Email: info@myherbalstore.com</p>
+    </div>
+  );
+};
+
+export default ContactPage;
